1f1710e7200fd2412925315caacd02c001784ceb
Fake.User <russellboswell@protonmail.com>
2025-03-02 11:21:09 -0500

    proper inline

--- util/build.js
+++ util/build.js
@@ -6,17 +6,17 @@ const path = dirname(dirname(fileURLToPath(import.meta.url)));
 mkdirSync(`${path}/dist`, {recursive: true, force: true});
 
 let indexContent = readFileSync(`${path}/src/index.html`, "utf-8")
-    .replace(`"ogcourier.woff2"`, `data:font/woff2;base64,${readFileSync(`${path}/src/ogcourier.woff2`).toString("base64")}`)
-    .replace(`"basilisk.gif"`, `data:image/gif;base64,${readFileSync(`${path}/src/basilisk.gif`).toString("base64")}`)
-    .replace(`"favicon.png"`, `data:image/png;base64,${readFileSync(`${path}/src/favicon.png`).toString("base64")}`)
-    .replace(`"cursor.webp"`, `data:image/gif;base64,${readFileSync(`${path}/src/cursor.webp`).toString("base64")}`)
-    .replace(`"mask.png"`, `data:image/gif;base64,${readFileSync(`${path}/src/mask.png`).toString("base64")}`);
+    .replace(`url("ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/ogcourier.woff2`).toString("base64")}")`)
+    .replace('src="basilisk.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/basilisk.gif`).toString("base64")}"`)
+    .replace(`href="favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/favicon.png`).toString("base64")}"`)
+    .replace(`url("cursor.webp")`, `url("data:image/gif;base64,${readFileSync(`${path}/src/cursor.webp`).toString("base64")}")`)
+    .replace(`src="mask.png"`, `src="data:image/gif;base64,${readFileSync(`${path}/src/mask.png`).toString("base64")}"`);
 writeFileSync(`${path}/dist/index.html`, indexContent, {recursive: true, force: true});
 
 let submissionsContent = readFileSync(`${path}/src/submissions.html`, "utf-8")
-    .replace(`"ogcourier.woff2"`, `data:font/woff2;base64,${readFileSync(`${path}/src/ogcourier.woff2`).toString("base64")}`)
-    .replace(`<script src="sqlite3.js"></script>`, `<script>${readFileSync(`${path}/src/sqlite3.js`, "utf-8")}</script>`)
-    .replace(`"favicon.png"`, `data:image/png;base64,${readFileSync(`${path}/src/favicon.png`).toString("base64")}`);
+    .replace(`url("ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/ogcourier.woff2`).toString("base64")}")`)
+    .replace(`href="favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/favicon.png`).toString("base64")}"`)
+    .replace('<script src="sqlite3.js"></script>', `<script>${readFileSync(`${path}/src/sqlite3.js`, "utf-8")}</script>`)
 writeFileSync(`${path}/dist/submissions.html`, submissionsContent, {recursive: true, force: true});
 
 cpSync(`${path}/src/basilisk.blend.zip`, `${path}/dist/basilisk.blend.zip`, {recursive: true, force: true});
