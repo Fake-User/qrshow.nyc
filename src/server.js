7dc62cf5b40054edcf70ac6ccf7816ad90b6c568
Fake.User <russellboswell@protonmail.com>
2025-03-19 14:31:18 -0700

    init retrospective

--- src/server.js
+++ src/server.js
@@ -1,61 +1,7 @@
-import { Database } from "bun:sqlite";
-import { S3Client, file} from "bun";
+/* globals */
 const dbPath = `${process.env.STORE}/db.sqlite`;
 
-/* db */
-const db = new Database(dbPath);
-db.run(`
-    CREATE TABLE IF NOT EXISTS submissions (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR(64) NOT NULL,
-        role VARCHAR(10) NOT NULL,
-        email VARCHAR(254) NOT NULL,
-        link VARCHAR(512) NOT NULL,
-        category VARCHAR(8) NOT NULL,
-        medium VARCHAR(64) NOT NULL,
-        title VARCHAR(512) NOT NULL,
-        author VARCHAR(254),
-        notes VARCHAR(512),
-        date DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-`);
-const preparedsStatement = db.prepare("INSERT INTO submissions (name, role, email, link, category, medium, title, author, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
-async function dbInsert(req){
-    try{
-        const formData = await req.formData();
-        console.log(formData);
-        const name = (formData.get("name")?.slice(0, 64) || "-");
-        const role = (formData.get("role")?.slice(0, 10) || "-");
-        const email = (formData.get("email")?.slice(0, 254) || "-");
-        const link = (formData.get("link")?.slice(0, 512) || "-");
-        const category = (formData.get("category")?.slice(0, 8) || "-");
-        const medium = (formData.get("medium")?.slice(0, 64) || "-");
-        const title = (formData.get("title")?.slice(0, 512) || "-");
-        const author = (formData.get("author")?.slice(0, 254) || "-");
-        const notes = (formData.get("notes")?.slice(0, 512) || "-");
-        preparedsStatement.run(name, role, email, link, category, medium, title, author, notes);
-        return new Response("success", {status: 200});
-    }
-    catch{
-        return new Response("error", {status: 400});
-    };
-};
-
-/* backup */
-const client = new S3Client({
-    accessKeyId: process.env.R2_KEY,
-    secretAccessKey: process.env.R2_SECRET,
-    bucket: "qrshow-backup",
-    endpoint: `https://${process.env.R2_ACCOUNT}.r2.cloudflarestorage.com`,
-});
-async function backup(){
-    const timestamp = new Date().toISOString();
-    await client.write(`backup-${timestamp}.sqlite`, file(dbPath));
-};
-setInterval(backup, 1800000); /* 30min */
-
 /* files */
-
 const fileSubmissions = Bun.file(`./submissions.html`);
 const fileBasilisk = Bun.file(`./basilisk.blend.zip`);
 const fileCube = Bun.file(`./cube-f4dbdc79c695.gif`);
@@ -76,7 +22,6 @@ const server = Bun.serve({
         /* main */
         if(path === `/${process.env.SECURITY_MORE_LIKE_OBSCURITY}`){return new Response(fileSubmissions, {headers: {"Content-Type": "text/html"}})};
         if(path === "/" || path === "/index.html"){return new Response(fileIndex, {headers: {"Content-Type": "text/html"}})};
-        if(path === "/submit" && req.method === "POST"){return dbInsert(req)};
 
         /* files */
         if(path === `/${process.env.SECURITY_MORE_LIKE_OBSCURITY}/store/db.sqlite`){return new Response(Bun.file(dbPath), {headers: {"Content-Type": "application/x-sqlite"}})};
