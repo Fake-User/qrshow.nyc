f7041b1d06d57fa6f37ef699413b89ff65334233
Fake.User <russellboswell@protonmail.com>
2025-03-28 19:23:07 -0700

    favicon gif

--- util/build.js
+++ util/build.js
@@ -10,7 +10,7 @@ let indexContent = readFileSync(`${path}/src/index.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
     .replace('src="/assets/basilisk.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/basilisk.gif`).toString("base64")}"`)
     .replace(`url("/assets/cursor.webp")`, `url("data:image/gif;base64,${readFileSync(`${path}/src/assets/cursor.webp`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace(`src="/assets/mask.png"`, `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/mask.png`).toString("base64")}"`);
 writeFileSync(`${path}/dist/index.html`, indexContent, {recursive: true, force: true});
 
@@ -19,7 +19,7 @@ let retrospectiveContent = readFileSync(`${path}/src/retrospective.html`, "utf-8
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
     .replace(`/assets/balanced-breakfast.png`, `data:image/png;base64,${readFileSync(`${path}/src/assets/balanced-breakfast.png`).toString("base64")}`)
     .replace(`/assets/quasi-recursion.png`, `data:image/png;base64,${readFileSync(`${path}/src/assets/quasi-recursion.png`).toString("base64")}`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace(`/assets/receipt-loom.png`, `data:image/png;base64,${readFileSync(`${path}/src/assets/receipt-loom.png`).toString("base64")}`)
     .replace(`/assets/qr-tortilla.png`, `data:image/png;base64,${readFileSync(`${path}/src/assets/qr-tortilla.png`).toString("base64")}`)
     .replace(`/assets/god-encoder.png`, `data:image/png;base64,${readFileSync(`${path}/src/assets/god-encoder.png`).toString("base64")}`)
@@ -44,29 +44,30 @@ writeFileSync(`${path}/dist/retrospective.html`, retrospectiveContent, {recursiv
 let rabbitholeContent = readFileSync(`${path}/src/rabbithole.html`, "utf-8")
     .replace(`url("/assets/cursor-small.webp")`, `url("data:image/gif;base64,${readFileSync(`${path}/src/assets/cursor-small.webp`).toString("base64")}")`)
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace('src="/assets/hellcat.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/hellcat.gif`).toString("base64")}"`)
     .replace(`src="/assets/next.png"`, `src="data:image/png;base64,${readFileSync(`${path}/src/assets/next.png`).toString("base64")}"`)
 writeFileSync(`${path}/dist/rabbithole.html`, rabbitholeContent, {recursive: true, force: true});
 
 let submissionsContent = readFileSync(`${path}/src/submissions.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace('<script src="sqlite3.js"></script>', `<script>${readFileSync(`${path}/src/sqlite3.js`, "utf-8")}</script>`)
 writeFileSync(`${path}/dist/submissions.html`, submissionsContent, {recursive: true, force: true});
 
 let escapeContent = readFileSync(`${path}/src/escape.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace('src="/assets/computer.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/computer.gif`).toString("base64")}"`)
+    .replace(`href="/assets/qrshow.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/qrshow.png`).toString("base64")}"`)
     .replace(`src="/assets/recurse.png"`, `src="data:image/png;base64,${readFileSync(`${path}/src/assets/recurse.png`).toString("base64")}"`)
-    .replace(`src="/assets/favicon.png"`, `src="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`src="/assets/favicon.gif"`, `src="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace(`src="/assets/email.png"`, `src="data:image/png;base64,${readFileSync(`${path}/src/assets/email.png`).toString("base64")}"`)
 writeFileSync(`${path}/dist/escape.html`, escapeContent, {recursive: true, force: true});
 
 let trifectaContent = readFileSync(`${path}/src/trifecta.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace('src="/assets/spine-3.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/spine-3.gif`).toString("base64")}"`)
     .replace('src="/assets/spine-4.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/spine-4.gif`).toString("base64")}"`)
     .replace('src="/assets/spine-5.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/spine-5.gif`).toString("base64")}"`)
@@ -75,13 +76,13 @@ writeFileSync(`${path}/dist/trifecta.html`, trifectaContent, {recursive: true, f
 
 let finaleContent = readFileSync(`${path}/src/finale.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
     .replace('src="/assets/basilisk.gif"', `src="data:image/gif;base64,${readFileSync(`${path}/src/assets/basilisk.gif`).toString("base64")}"`)
 writeFileSync(`${path}/dist/finale.html`, finaleContent, {recursive: true, force: true});
 
 let aloneContent = readFileSync(`${path}/src/alone.html`, "utf-8")
     .replace(`url("/assets/ogcourier.woff2")`, `url("data:font/woff2;base64,${readFileSync(`${path}/src/assets/ogcourier.woff2`).toString("base64")}")`)
-    .replace(`href="/assets/favicon.png"`, `href="data:image/png;base64,${readFileSync(`${path}/src/assets/favicon.png`).toString("base64")}"`)
+    .replace(`href="/assets/favicon.gif"`, `href="data:image/gif;base64,${readFileSync(`${path}/src/assets/favicon.gif`).toString("base64")}"`)
 writeFileSync(`${path}/dist/alone.html`, aloneContent, {recursive: true, force: true});
 
 cpSync(`${path}/src//assets/cube-f4dbdc79c695.gif`, `${path}/dist/cube-f4dbdc79c695.gif`, {recursive: true, force: true});
